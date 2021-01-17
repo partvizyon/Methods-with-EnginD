ac5e531be0eccc2ea30caabe4dae8d5c42d84b75
partvizyon <elawhitestone@gmail.com>
2021-01-17 03:50:56 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Methods
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string urunAdi = "Elma";
+         double fiyati = 15;
+         string aciklama = "Amasya Elması";
+
+         string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+         Urun urun1 = new Urun();
+         urun1.Adi = "Elma";
+         urun1.Fiyati = 15;
+         urun1.Aciklama = "Amasya Elma";
+
+         Urun urun2 = new Urun();
+         urun2.Adi = "Karpuz";
+         urun2.Fiyati = 80;
+         urun2.Aciklama = "Diyarbakır Karpuz";
+
+         Urun urun3 = new Urun();
+         urun3.Adi = "Çilek";
+         urun3.Fiyati = 25;
+         urun3.Aciklama = "Akdeniz Çilek";
+
+         Urun[] urunler = new Urun[] { urun1, urun2, urun3 };
+
+         // type-safe -- tip güvenli
+         foreach (Urun urun in urunler)
+         {
+            Console.WriteLine(urun.Adi);
+            Console.WriteLine(urun.Fiyati);
+            Console.WriteLine(urun.Aciklama);
+            Console.WriteLine("--------------");
+         }
+
+         Console.WriteLine("---------Methods---------");
+
+         // Instance - Ornek
+         // Encapsulation
+         SepetManager sepetManager = new SepetManager();
+
+         sepetManager.Ekle(urun1);
+         sepetManager.Ekle(urun2);
+         sepetManager.Ekle(urun3);
+         
+         sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 20);
+         sepetManager.Ekle2("Elma", "Yeşil Elma", 20, 40);
+         sepetManager.Ekle2("Karpuz", "Kesmece Karpuz", 80, 70);
+      }
+   }
+}
+
+// Don't repeat yourself - DRY
+// Clean Code
+// Best Practice
---
+++ ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ValueAndReferenceTypes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         int sayi1 = 10;
+         int sayi2 = 30;
+         sayi1 = sayi2;
+         sayi2 = 65;
+         //sayi1 ??  Cevap:30
+
+         Console.WriteLine(sayi1);
+         // int, decimal, float, double, bool 0 değer tiptir.
+
+         int[] sayilar1 = new int[] { 10, 20, 30 };
+         int[] sayilar2 = new int[] { 100, 200, 300 };
+         sayilar1 = sayilar2;
+         sayilar2[0] = 999;
+
+         Console.WriteLine(sayilar2[0]);
+         //sayilar[0]  Cevap 999
+
+         // array, class, interface = referans tip.
+         // Bellekte stasck ve heap şeklinde 2 alan var.
+         // Değer tip olanlar STACK te tanımlanır.
+
+         // Referans tipler ise Heap'te tanımlanır.
+      }
+   }
+}
